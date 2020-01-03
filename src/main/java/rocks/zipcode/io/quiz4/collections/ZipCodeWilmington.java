816107d6020e61943c55eb8d4e999afe5b1d2c25
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2020-01-03 10:30:13 -0500

    enroll works

--- src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
@@ -1,19 +1,30 @@
 package rocks.zipcode.io.quiz4.collections;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class ZipCodeWilmington {
+    ArrayList<Student> students;
+
+    public ZipCodeWilmington(){
+        this.students = new ArrayList<>();
+    }
+
     public void enroll(Student student) {
+        this.students.add(student);
     }
 
     public Boolean isEnrolled(Student student) {
-        return null;
+        return this.students.contains(student);
     }
 
     public void lecture(double numberOfHours) {
+        for(Student s : students){
+            s.studyTime += numberOfHours/students.size();
+        }
     }
 
     public Map<Student, Double> getStudyMap() {
