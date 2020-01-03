d74fae1fcb64191b84fa753ecc110982098d9f07
jeanvalentin51 <valentin.j.gheorghe.com>
2020-01-03 10:32:34 -0500

    is palindrome object

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
@@ -1,10 +1,14 @@
 package rocks.zipcode.io.quiz4.objectorientation;
-
+import rocks.zipcode.io.quiz4.fundamentals.*;
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeObject {
+
+    String string;
+
     public PalindromeObject(String input) {
+        string = input;
     }
 
     public String[] getAllPalindromes(){
@@ -12,7 +16,7 @@ public class PalindromeObject {
     }
 
     public Boolean isPalindrome(){
-        return null;
+        return PalindromeEvaluator.isPalindrome(string);
     }
 
     public String reverseString(){
