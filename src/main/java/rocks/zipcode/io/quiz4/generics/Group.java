04b0954d853dc825badc77ada7775f5b1f53b187
jeanvalentin51 <valentin.j.gheorghe.com>
2020-01-03 10:53:07 -0500

    integer, string group

--- src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
@@ -1,34 +1,42 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
  * @author leon on 18/12/2018.
  */
 public class Group<_> {
+
+    ArrayList<_> result = new ArrayList<>();
+
     public Group() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        //throw new UnsupportedOperationException("Method not yet implemented");
     }
 
     public Integer count() {
-        return null;
+
+        return result.size();
     }
 
     public void insert(_ value) {
+        result.add(value);
     }
 
     public Boolean has(_ value) {
-        return null;
+        return result.contains(value);
     }
 
     public _ fetch(int indexOfValue) {
-        return null;
+        return result.get(indexOfValue);
     }
 
     public void delete(_ value) {
+        result.remove(value);
     }
 
     public void clear() {
+        result.clear();
     }
 
     public Iterator<_> iterator() {
