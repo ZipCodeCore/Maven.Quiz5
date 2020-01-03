01c08f17294244b4da3a323d49fcf2d390e70c46
michael-doll <MWDoll88@gmail.com>
2020-01-03 11:51:37 -0500

    save, time -dnf

--- src/main/java/rocks/zipcode/io/quiz4/collections/SimpleStringGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/SimpleStringGroup.java
@@ -1,31 +1,50 @@
 package rocks.zipcode.io.quiz4.collections;
 
+import java.util.*;
+import java.util.function.Consumer;
+
 /**
  * @author leon on 11/12/2018.
  */
-public class SimpleStringGroup {
+public class SimpleStringGroup implements Iterable<String> {
+    List<String> asdf;
     public SimpleStringGroup() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        this.asdf = new ArrayList<>();
     }
 
     public Integer count() {
-        return null;
+        return asdf.size();
     }
 
-    public void insert(String string) {
+    public void insert(String string) { asdf.add(string);
     }
 
     public Boolean has(String string) {
-        return null;
+        return asdf.contains(string);
     }
 
     public String fetch(int indexOfValue) {
+        return asdf.get(indexOfValue);
+    }
+
+    public void delete(String string) { asdf.remove(string);
+    }
+
+    public void clear() { asdf.clear();
+    }
+
+    @Override
+    public Iterator<String> iterator() {
         return null;
     }
 
-    public void delete(String string) {
+    @Override
+    public void forEach(Consumer<? super String> action) {
+
     }
 
-    public void clear() {
+    @Override
+    public Spliterator<String> spliterator() {
+        return null;
     }
 }
