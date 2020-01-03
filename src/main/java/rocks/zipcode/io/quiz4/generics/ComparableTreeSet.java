a6c953154d41770633bff51a16f34fcdd2d3815b
jeanvalentin51 <valentin.j.gheorghe.com>
2020-01-03 14:35:54 -0500

    to string for comparable

--- src/main/java/rocks/zipcode/io/quiz4/generics/ComparableTreeSet.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/ComparableTreeSet.java
@@ -1,17 +1,30 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * @author leon on 11/12/2018.
  */
-public class    ComparableTreeSet<_> {
-    public ComparableTreeSet(_... arr) {
+public class    ComparableTreeSet<T> {
+
+    Set<T> mySet = new TreeSet<>();
+
+    public ComparableTreeSet(T... arr) {
+        mySet.addAll(Arrays.asList(arr));
     }
 
 
     public ComparableTreeSet() {
     }
 
-    public int compareTo(ComparableTreeSet<_> o) {
+    public int compareTo(ComparableTreeSet<T> o) {
         return Integer.valueOf(null);
+    }
+
+    @Override
+    public String toString() {
+        return mySet.toString();
     }
 }
