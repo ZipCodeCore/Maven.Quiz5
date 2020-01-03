da315be805c650d9d4f97554923bdb88fb2c6dee
Mike King <michkin32@gmail.com>
2020-01-03 11:22:58 -0500

    Worked on some generics

--- src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
@@ -1,37 +1,64 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
 /**
  * @author leon on 18/12/2018.
  */
-public class Group<_> {
+public class Group<_> implements Iterable{
+
+    ArrayList<_> list = new ArrayList<>();
     public Group() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+
     }
 
     public Integer count() {
-        return null;
+        return list.size();
     }
 
+
     public void insert(_ value) {
+        list.add(value);
     }
 
     public Boolean has(_ value) {
-        return null;
+        return list.contains(value);
     }
 
     public _ fetch(int indexOfValue) {
-        return null;
+        return list.get(indexOfValue);
     }
 
+
+
     public void delete(_ value) {
+        list.remove(value);
     }
 
     public void clear() {
+        list.clear();
     }
 
     public Iterator<_> iterator() {
         return null;
+    }
+
+    @Override
+    public void forEach(Consumer action) {
+
+    }
+
+    @Override
+    public Spliterator spliterator() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(list.toArray());
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
@@ -1,18 +1,30 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class SortedGroup<_> extends Group<_> {
+    private ArrayList<_> listOfValues = new ArrayList<>();
     @Override
     public void insert(_ value) {
+        listOfValues.add(value);
     }
 
     @Override
     public void delete(_ value) {
+        listOfValues.remove(value);
     }
 
     public Integer indexOf(_ value) {
-        return null;
+
+        for (int i =0; i < listOfValues.size(); i++){
+            if (listOfValues.get(i).equals(value)){
+                return i;
+            }
+        }
+            return -1;
     }
 }
