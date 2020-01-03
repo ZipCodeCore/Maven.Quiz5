41ab577cbc5743e702ce2dbd2dd0552bb07ade50
mike <mike.schoenbeck@gmail.com>
2020-01-03 10:07:36 -0500

    get all substrings completed

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,21 +1,23 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
 import java.util.HashSet;
+import java.util.TreeSet;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
     public static String[] getAllSubstrings(String string) {
-        HashSet<String> subs = new HashSet<>();
+        TreeSet<String> subs = new TreeSet<>();
         for(int i = 0; i< string.length(); i++) {
-            for (int j = 1; j<string.length(); j++) {
+            for (int j = i+1; j <=string.length(); j++) {
+
                 subs.add(string.substring(i, j));
             }
-        } subs.add(string);
-        for (int i = 0; i<string.length(); i++)   {
-            subs.add(string.substring(i,1));
         }
+//        for (int i = 0; i<string.length(); i++)   {
+//            subs.add(string.substring(i,1));
+//        }
         String[] subsArr = new String[subs.size()];
         subsArr = subs.toArray(subsArr);
         return subsArr;
