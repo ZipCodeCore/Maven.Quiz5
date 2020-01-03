2c658ac1ab15438a170061a6e48efab9902aec2e
Mike King <michkin32@gmail.com>
2020-01-03 10:33:43 -0500

    Collections and fundementals almost done

--- src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
@@ -1,22 +1,31 @@
 package rocks.zipcode.io.quiz4.collections;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class ZipCodeWilmington {
+    private Map<Student,Double> enrolledStudents = new HashMap<>();
+
     public void enroll(Student student) {
+        enrolledStudents.put(student,student.getTotalStudyTime());
     }
 
     public Boolean isEnrolled(Student student) {
-        return null;
+        return enrolledStudents.containsKey(student);
     }
 
     public void lecture(double numberOfHours) {
+        for (Student s : enrolledStudents.keySet()){
+            s.learn(numberOfHours);
+        }
+
     }
 
     public Map<Student, Double> getStudyMap() {
-        return null;
+        return enrolledStudents;
     }
 }
