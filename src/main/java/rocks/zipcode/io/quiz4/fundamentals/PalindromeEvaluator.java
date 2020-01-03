4607f3c4efda3e43c6c7b31ca349a13880241ea7
kshields412 <kshields412@gmail.com>
2020-01-03 10:08:01 -0500

    checkpoint

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,18 +1,40 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        Set<String> palindromes = new HashSet<>();
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i + 1; j <= string.length(); j++) {
+                if (isPalindrome(string.substring(i, j))) {
+                    palindromes.add(string.substring(i, j));
+                }
+            }
+        }
+        String[] strArr = new String[palindromes.size()];
+        int index = 0;
+        for (String str : palindromes) {
+            strArr[index++] = str;
+        }
+        return strArr;
     }
 
     public static Boolean isPalindrome(String string) {
-        return null;
+        String str = reverseString(string);
+        if(str.compareTo(string) == 0) {
+            return true;
+        } else
+            return false;
     }
 
     public static String reverseString(String string) {
-        return null;
+        StringBuilder sb = new StringBuilder().append(string);
+        sb.reverse();
+        return sb.toString();
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,11 +1,12 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
-    public static String[] getAllSubstrings(String string) {
-        return null;
+    public static String[] getAllSubstrings(String string) { return null;
     }
 
     public static String[] getCommonSubstrings(String string1, String string2) {
