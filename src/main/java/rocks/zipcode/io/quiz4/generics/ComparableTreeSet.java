e0169c3a9d9726dc9eda5630678aaf2b11604ef1
jeanvalentin51 <valentin.j.gheorghe.com>
2020-01-03 14:51:01 -0500

    generic poly

--- src/main/java/rocks/zipcode/io/quiz4/generics/ComparableTreeSet.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/ComparableTreeSet.java
@@ -7,7 +7,7 @@ import java.util.TreeSet;
 /**
  * @author leon on 11/12/2018.
  */
-public class    ComparableTreeSet<T> {
+public class ComparableTreeSet<T> extends TreeSet<T>  implements Comparable<T>{
 
     Set<T> mySet = new TreeSet<>();
 
@@ -26,5 +26,10 @@ public class    ComparableTreeSet<T> {
     @Override
     public String toString() {
         return mySet.toString();
+    }
+
+    @Override
+    public int compareTo(T o) {
+        return 0;
     }
 }
