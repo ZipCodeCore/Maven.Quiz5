a8c812a9e8f83426c49d974228154dfd85741a56
SandyPham823 <sandydupham@gmail.com>
2020-01-03 11:49:22 -0500

    your friendly neighborhood commit

--- src/main/java/rocks/zipcode/io/quiz4/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/Student.java
@@ -4,17 +4,22 @@ package rocks.zipcode.io.quiz4.collections;
  * @author leon on 11/12/2018.
  */
 public class Student {
+Integer id;
+Double amountOfHours;
+
     public Student() {
         this(null);
     }
 
     public Student(Integer id) {
+        this.id = id;
     }
 
     public void learn(Double amountOfHours) {
+        this.amountOfHours = amountOfHours;
     }
 
     public Double getTotalStudyTime() {
-        return null;
+        return amountOfHours;
     }
 }
