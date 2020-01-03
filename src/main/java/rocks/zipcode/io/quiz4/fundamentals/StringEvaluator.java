db7dea9262140638c1d10a62b5ab098e6c7109b0
Brandon Chambers <brandonmchambers36@gmail.com>
2020-01-03 11:51:56 -0500

    deadline

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,19 +1,49 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
     public static String[] getAllSubstrings(String string) {
 
-        if(string.length() == 1){
+        if(string.length() == 1) {
             String[] str = new String[1];
-            for(int i = 0; i < 1; i++){
+            for (int i = 0; i < 1; i++) {
                 str[i] = string;
                 return str;
             }
         }
-        return null;
+
+        ArrayList<String> strList = new ArrayList<>();
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i+1; j <= string.length(); j++) {
+                   strList.add(string.substring(i,j));
+            }
+            }
+
+        String[] str2 = new String[strList.size()];
+
+        for (int j = 0; j < strList.size(); j++) {
+
+            str2[j] = strList.get(j);
+        }
+
+        Set<String> set = new HashSet<>(Arrays.asList(str2));
+
+
+        int n = set.size();
+        String[] arrStr = new String[n];
+
+        int i = 0;
+        for (String x : set)
+            arrStr[i++] = x;
+
+    return arrStr;
     }
 
     public static String[] getCommonSubstrings(String string1, String string2) {
