516aae2672f22c3b7529fda993b233a19605fd0e
mike <mike.schoenbeck@gmail.com>
2020-01-03 11:45:41 -0500

    completed palindrom and evaluator in object oriented

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringEvaluatorObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringEvaluatorObject.java
@@ -1,21 +1,35 @@
 package rocks.zipcode.io.quiz4.objectorientation;
 
+import rocks.zipcode.io.quiz4.fundamentals.StringEvaluator;
+
 /**
  * @author leon on 19/12/2018.
  */
 public class StringEvaluatorObject {
+    String str;
+    String secondInput;
+
     public StringEvaluatorObject(String str) {
+        this.str = str;
+    }
+
+    public StringEvaluatorObject(String str, String secondInput) {
+        this.str = str;
+        this.secondInput = this.secondInput;
     }
 
     public String[] getAllSubstrings() {
-        return null;
+
+        return StringEvaluator.getAllSubstrings(str);
     }
 
     public String[] getCommonSubstrings(String secondInput) {
-        return null;
+        return StringEvaluator.getCommonSubstrings(str, secondInput);
     }
 
     public String getLargestCommonSubstring(String secondInput) {
-        return null;
+        String largest = StringEvaluator.getLargestCommonSubstring(str, secondInput);
+
+        return largest;
     }
 }
