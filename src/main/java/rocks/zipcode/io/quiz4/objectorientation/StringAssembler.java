d645683c787a85799e736c5b54465b35fcb9d8b1
Kievina <56592501+Kievina@users.noreply.github.com>
2020-01-03 11:37:35 -0500

    progress made in obj orientation

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
@@ -4,14 +4,47 @@ package rocks.zipcode.io.quiz4.objectorientation;
  * @author leon on 11/12/2018.
  */
 public class StringAssembler {
+    private Character origDelimeter;
+    private String newDelimeter;
+    private String string;
+
+    public Character getOrigDelimeter() {
+        return origDelimeter;
+    }
+
+    public void setOrigDelimeter(Character origDelimeter) {
+        this.origDelimeter = origDelimeter;
+    }
+
+    public String getNewDelimeter() {
+        return String.valueOf(origDelimeter);
+    }
+
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
     public StringAssembler(Character delimeter) {
+        this.origDelimeter = delimeter;
+        this.newDelimeter = String.valueOf(delimeter);
+        this.string = "";
     }
 
     public StringAssembler append(String str) {
-        return null;
+        StringAssembler sa = new StringAssembler(origDelimeter);
+        sa.setString(str);
+        return sa;
     }
 
     public String assemble() {
+//StringBuilder sb = new StringBuilder();
+//sb.append()
+//        sb.append()
         return null;
     }
 }
