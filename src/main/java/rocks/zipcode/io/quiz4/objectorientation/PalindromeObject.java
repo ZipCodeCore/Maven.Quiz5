0026cae6a1e90c5d39f07cc2fc4324a1ee4736f1
SandyPham823 <sandydupham@gmail.com>
2020-01-03 11:31:38 -0500

    got most of section 2 done, moving to 3

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
@@ -4,7 +4,10 @@ package rocks.zipcode.io.quiz4.objectorientation;
  * @author leon on 18/12/2018.
  */
 public class PalindromeObject {
+    String input;
+
     public PalindromeObject(String input) {
+        this.input = input;
     }
 
     public String[] getAllPalindromes(){
@@ -12,10 +15,31 @@ public class PalindromeObject {
     }
 
     public Boolean isPalindrome(){
+        int i = 0, j = input.length() - 1;
+        while (i < j) {
+            if (input.charAt(i) != input.charAt(j))
+                return false;
+
+            i++;
+            j++;
+            return true;
+        }
         return null;
     }
 
     public String reverseString(){
-        return null;
+        char[] reversedString = input.toCharArray();
+        int start = 0;
+        int end = reversedString.length - 1;
+        char temp;
+
+        while (end > start) {
+            temp = reversedString[start];
+            reversedString[start] = reversedString[end];
+            reversedString[end] = temp;
+            end--;
+            start++;
+        }
+        return new String(reversedString);
     }
 }
