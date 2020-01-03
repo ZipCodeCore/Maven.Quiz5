da5d2f952a791fc02efc31f7828289e2bcc9ed40
jeanvalentin51 <valentin.j.gheorghe.com>
2020-01-03 10:04:19 -0500

    Enroll student

--- src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
@@ -1,16 +1,21 @@
 package rocks.zipcode.io.quiz4.collections;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class ZipCodeWilmington {
+    ArrayList<Student> allStudents = new ArrayList<>();
+
+
     public void enroll(Student student) {
+        allStudents.add(student);
     }
 
     public Boolean isEnrolled(Student student) {
-        return null;
+        return allStudents.contains(student);
     }
 
     public void lecture(double numberOfHours) {
