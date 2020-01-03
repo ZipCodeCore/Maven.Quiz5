ddfa7991adb9e8ee6e164a6b117bd5588eb85ce8
wes <wes.jones1597@gmail.com>
2020-01-03 10:23:13 -0500

    fundamentals

--- src/main/java/rocks/zipcode/io/quiz4/collections/SimpleStringGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/SimpleStringGroup.java
@@ -1,31 +1,69 @@
 package rocks.zipcode.io.quiz4.collections;
+import java.util.Iterator;
 
 /**
  * @author leon on 11/12/2018.
  */
-public class SimpleStringGroup {
+public class SimpleStringGroup implements Iterable {
+    private String[] group;
     public SimpleStringGroup() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        this.group = new String[0];
+//        throw new UnsupportedOperationException("Method not yet implemented");
     }
 
+    public String[] getArr() { return this.group; }
+
     public Integer count() {
-        return null;
+        return this.group.length;
     }
 
     public void insert(String string) {
+        String[] group = new String[this.count()+1];
+
+        for (int i = 0; i < this.group.length; i++)
+            if (this.group.length > 0)
+                group[i] = this.group[i];
+
+        group[group.length-1] = string;
+        this.group = group;
     }
 
     public Boolean has(String string) {
-        return null;
+        for (String word : this.group) {
+            if (string.equals(word)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public String fetch(int indexOfValue) {
-        return null;
+        if (indexOfValue < this.count())
+            return this.group[indexOfValue];
+        else
+            throw new IndexOutOfBoundsException();
     }
 
     public void delete(String string) {
+        String[] group = new String[this.group.length-1];
+        boolean deleted = false;
+        for (int i = 0; i < this.group.length; i++) {
+            if (!this.group[i].equals(string)) {
+                if (!deleted) group[i] = this.group[i];
+                else group[i-1] = this.group[i];
+            }
+            else deleted = true;
+        }
+        this.group = group;
     }
 
     public void clear() {
+        this.group = new String[0];
+    }
+
+
+    @Override
+    public Iterator<String> iterator() {
+        return null;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,18 +1,42 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import rocks.zipcode.io.quiz4.collections.SimpleStringGroup;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
     public static String[] getAllSubstrings(String string) {
-        return null;
+        SimpleStringGroup group = new SimpleStringGroup();
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i+1; j < string.length()+1; j++) {
+                if (!group.has(string.substring(i, j)))
+                    group.insert(string.substring(i, j));
+            }
+        }
+        return group.getArr();
     }
 
     public static String[] getCommonSubstrings(String string1, String string2) {
-        return null;
+        SimpleStringGroup group = new SimpleStringGroup();
+        String[] gr1 = getAllSubstrings(string1);
+
+        for (int i = 0; i < gr1.length; i++) {
+            if (string2.contains(gr1[i]) && !group.has(gr1[i])) {
+                group.insert(gr1[i]);
+            }
+        }
+        return group.getArr();
     }
 
     public static String getLargestCommonSubstring(String string1, String string2) {
-        return null;
+        String[] common = getCommonSubstrings(string1, string2);
+        String lgCommon = "";
+        for (String word : common) {
+            if (word.length() > lgCommon.length()) {
+                lgCommon = word;
+            }
+        }
+        return lgCommon;
     }
 }
