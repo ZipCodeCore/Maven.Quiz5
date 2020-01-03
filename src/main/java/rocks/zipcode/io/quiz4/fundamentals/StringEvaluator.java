1d5df5e9518aebdb8dfc84d33c84e1b75cb5c0a0
mike <mike.schoenbeck@gmail.com>
2020-01-03 09:51:01 -0500

    quick save to pull to zcoder

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,11 +1,24 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.HashSet;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
     public static String[] getAllSubstrings(String string) {
-        return null;
+        HashSet<String> subs = new HashSet<>();
+        for(int i = 0; i< string.length(); i++) {
+            for (int j = 1; j<string.length(); j++) {
+                subs.add(string.substring(i, j));
+            }
+        } subs.add(string);
+        for (int i = 0; i<string.length(); i++)   {
+            subs.add(string.substring(i,1));
+        }
+        String[] subsArr = new String[subs.size()];
+        subsArr = subs.toArray(subsArr);
+        return subsArr;
     }
 
     public static String[] getCommonSubstrings(String string1, String string2) {
