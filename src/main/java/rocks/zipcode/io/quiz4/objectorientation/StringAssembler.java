625cf039746792985bb1db383290f2b23b1bf0d7
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2020-01-03 11:00:04 -0500

    stack finished

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
@@ -1,13 +1,17 @@
 package rocks.zipcode.io.quiz4.objectorientation;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringAssembler {
     Character delimeter;
+    ArrayList<String> stringList;
 
     public StringAssembler(Character delimeter) {
         this.delimeter = delimeter;
+        this.stringList = new ArrayList<>();
     }
 
     public StringAssembler append(String str) {
