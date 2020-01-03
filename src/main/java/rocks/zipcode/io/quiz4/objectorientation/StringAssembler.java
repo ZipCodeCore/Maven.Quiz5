dbb4018cc30d63148acf9ead0313c3b0b44f64fc
wes <wes.jones1597@gmail.com>
2020-01-03 10:33:25 -0500

    objectorientation

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
@@ -1,17 +1,32 @@
 package rocks.zipcode.io.quiz4.objectorientation;
 
+import rocks.zipcode.io.quiz4.collections.SimpleStringGroup;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringAssembler {
+    private Character delimeter;
+    private SimpleStringGroup group;
     public StringAssembler(Character delimeter) {
+        this.group = new SimpleStringGroup();
+        this.delimeter = delimeter;
     }
 
     public StringAssembler append(String str) {
-        return null;
+        this.group.insert(str);
+        return this;
     }
 
     public String assemble() {
-        return null;
+        String result = "";
+        for (String word : this.group.getArr()) {
+            result = make(result, word);
+        }
+        return result;
+    }
+
+    public String make (String original, String add) {
+        return original + (original.length() > 0 ? this.delimeter : "") + add;
     }
 }
