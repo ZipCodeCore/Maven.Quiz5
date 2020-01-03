52adde5a88aace2d3349355a9b4d796535abf3c4
jeanvalentin51 <valentin.j.gheorghe.com>
2020-01-03 09:30:35 -0500

    get all substrings

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,11 +1,35 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
+
+    static int counter = 0;
+
     public static String[] getAllSubstrings(String string) {
-        return null;
+
+        HashSet<String> temp = new HashSet<>();
+        String [] word = string.split("");
+        String interim = "";
+        String [] result;
+
+        for (int i = 0; i < word.length; i++){
+            interim = "";
+            temp.add(word[i]);
+            interim = word[i];
+            counter++;
+            for (int k = i + 1; k < word.length; k ++){
+                interim = interim.concat(word[k]);
+                temp.add(interim);
+                counter++;
+            }
+        }
+        result = new String[temp.size()];
+        return temp.toArray(result);
     }
 
     public static String[] getCommonSubstrings(String string1, String string2) {
