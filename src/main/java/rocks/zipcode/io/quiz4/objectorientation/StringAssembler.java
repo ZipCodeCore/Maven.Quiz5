d3aa0dcaa8f3400c49120d40c2aac5fa8c9ca04a
jeanvalentin51 <valentin.j.gheorghe.com>
2020-01-03 10:40:03 -0500

    string assembler

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
@@ -4,14 +4,19 @@ package rocks.zipcode.io.quiz4.objectorientation;
  * @author leon on 11/12/2018.
  */
 public class StringAssembler {
+    Character delimiter;
+    String result = "";
+
     public StringAssembler(Character delimeter) {
+        this.delimiter = delimeter;
     }
 
     public StringAssembler append(String str) {
-        return null;
+        result = result.concat(str).concat(delimiter.toString());
+        return this;
     }
 
     public String assemble() {
-        return null;
+        return result.substring(0,result.length()-1);
     }
 }
