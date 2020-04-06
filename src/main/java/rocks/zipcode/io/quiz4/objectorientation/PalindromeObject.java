9ba796f5c0fd1e83bcbd19929675c8a1c8ad2cbd
cwilliams <coreywllms9@gmail.com>
2020-04-06 11:53:36 -0400

    string evaluator

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
@@ -1,41 +1,29 @@
 package rocks.zipcode.io.quiz4.objectorientation;
 
+import rocks.zipcode.io.quiz4.fundamentals.PalindromeEvaluator;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeObject {
+
+    private String word;
     public PalindromeObject(String input) {
+        this.word = input;
     }
 
     public String[] getAllPalindromes(){
-        return null;
+        return PalindromeEvaluator.getAllPalindromes(word);
     }
 
     public Boolean isPalindrome(){
-        String clean = "".replaceAll("\\s+", "").toLowerCase();
-        int length = clean.length();
-        int forward = 0;
-        int backward = length - 1;
-        while (backward > forward) {
-            char forwardChar = clean.charAt(forward++);
-            char backwardChar = clean.charAt(backward--);
-            if (forwardChar != backwardChar)
-                return false;
-        }
-        return true;
 
-       // return null;
-
+        return PalindromeEvaluator.isPalindrome(word);
     }
 
     public String reverseString(){
-        StringBuilder reverse = new StringBuilder();
-        String clean = "".replaceAll("\\s+", "").toLowerCase();
-        char[] plain = clean.toCharArray();
-        for (int i = plain.length - 1; i >= 0; i--) {
-            reverse.append(plain[i]);
-        }
-        return String.valueOf((reverse.toString()).equals(clean));
-        //return null;
+
+        return PalindromeEvaluator.reverseString(word);
+
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
@@ -4,7 +4,12 @@ package rocks.zipcode.io.quiz4.objectorientation;
  * @author leon on 11/12/2018.
  */
 public class StringAssembler {
+
+    private Character delimeter;
+    String assembled;
+
     public StringAssembler(Character delimeter) {
+        this.delimeter = delimeter;
     }
 
     public StringAssembler append(String str) {
