44a7957f09ea88a0fc9d2809025759f61fc9b55f
vle <obentoka@gmail.com>
2020-04-06 10:58:16 -0400

    initial commit

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -5,14 +5,31 @@ package rocks.zipcode.io.quiz4.fundamentals;
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        String[] allPali = new String[0];
+        String[] allSub = StringEvaluator.getAllSubstrings(string);
+        for(String s : allSub){
+            if(isPalindrome(s))
+                allPali = insert(allPali, s);
+        }
+        return allPali;
     }
 
     public static Boolean isPalindrome(String string) {
-        return null;
+        return string.equals(reverseString(string));
     }
 
     public static String reverseString(String string) {
-        return null;
+        StringBuilder revString = new StringBuilder(string);
+        revString.reverse();
+        return revString.toString();
+    }
+
+    public static String[] insert(String[] input, String string){
+        String[] retArr = new String[input.length+1];
+        for(int i = 0; i < input.length; i++){
+            retArr[i] = input[i];
+        }
+        retArr[input.length] = string;
+        return retArr;
     }
 }
