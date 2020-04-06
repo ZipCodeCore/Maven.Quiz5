70ea09f0aab60377005a294ae7d15248d26bcf96
chipfody <60615532+chipfody@users.noreply.github.com>
2020-04-06 10:05:18 -0400

    save1

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,18 +1,45 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
     public static String[] getAllSubstrings(String string) {
-        return null;
+        List<String> subStrings = new ArrayList<> ();
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i + 1; j < string.length() + 1; j++){
+                if (!subStrings.contains(string.substring(i, j)))
+                    subStrings.add(string.substring(i, j));
+            }
+        }
+        return subStrings.toArray(new String[0]);
     }
 
     public static String[] getCommonSubstrings(String string1, String string2) {
-        return null;
+        String[] array1 = getAllSubstrings ( string1 );
+        String[] array2 = getAllSubstrings ( string2 );
+        List<String> commonSubs = new ArrayList<> (  );
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i].equals ( array2[j] ) ) {
+                    commonSubs.add ( array2[j] );
+                }
+            }
+        }
+        return commonSubs.toArray ( new String[0] );
     }
 
     public static String getLargestCommonSubstring(String string1, String string2) {
-        return null;
+        String[] baseArray = getCommonSubstrings ( string1, string2 );
+        String largestCommon = baseArray[0];
+        for (int i = 1; i < baseArray.length; i++) {
+            if (baseArray[i].length () > largestCommon.length ()) {
+                largestCommon = baseArray[i];
+            }
+        }
+        return largestCommon;
     }
 }
