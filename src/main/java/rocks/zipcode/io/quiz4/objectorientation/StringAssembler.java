b8251bb2fa5afae3ec764f9d7c8a228fabe6b6b9
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-04-06 13:00:55 -0400

    this is the work to be graded. Will finish quiz later for practice

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
@@ -1,23 +1,33 @@
 package rocks.zipcode.io.quiz4.objectorientation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringAssembler {
 
-    String string = "";
+    List<String> listOfStrings = new ArrayList<>();
     Character delimeter;
+    String result;
 
     public StringAssembler(Character delimeter) {
         this.delimeter = delimeter;
     }
 
     public StringAssembler append(String str) {
-        
+        listOfStrings.add(str);
         return null;
     }
 
     public String assemble() {
-        return null;
+        for (int i = 0; i < listOfStrings.size(); i++) {
+            result += listOfStrings.get(i);
+            if (i != listOfStrings.size()) {
+                result += delimeter;
+            }
+        }
+        return result;
     }
 }
