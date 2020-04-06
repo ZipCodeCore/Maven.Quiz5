cfa0e0b38f18798871e7ab16f5f9e99a0bae3077
Mo Russ <mauriceruss302@gmail.com>
2020-04-06 12:52:20 -0400

    9 zipCode tests passed

--- src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
@@ -1,19 +1,31 @@
 package rocks.zipcode.io.quiz4.collections;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class ZipCodeWilmington {
+    ArrayList<Student> students;
+
+    public ZipCodeWilmington() {
+        this.students = new ArrayList<Student>();
+    }
+
     public void enroll(Student student) {
+        this.students.add(student);
     }
 
     public Boolean isEnrolled(Student student) {
-        return null;
+
+        return this.students.contains(student);
     }
 
     public void lecture(double numberOfHours) {
+        for(Student s : students){
+            s.studyTime += numberOfHours;
+        }
     }
 
     public Map<Student, Double> getStudyMap() {
