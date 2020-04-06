73e0b1421c3cf4381bdb33141c303d4ef8eecfbf
k-romero <imkevinromero@gmail.com>
2020-04-06 10:53:47 -0400

    checkpoint

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,18 +1,25 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.stream.Stream;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        String[] subs = StringEvaluator.getAllSubstrings(string);
+         return Stream.of(subs).filter(PalindromeEvaluator::isPalindrome).toArray(String[]::new);
     }
 
     public static Boolean isPalindrome(String string) {
-        return null;
+        return string.equals(reverseString(string));
     }
 
     public static String reverseString(String string) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for(int i = string.length()-1; i >= 0 ; i--) {
+            builder.append(string.charAt(i));
+        }
+        return builder.toString();
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,11 +1,22 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
     public static String[] getAllSubstrings(String string) {
-        return null;
+        ArrayList<String> tempList = new ArrayList<>();
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i+1; j <= string.length() ; j++) {
+                String temp = string.substring(i,j);
+                if(!tempList.contains(temp)){
+                    tempList.add(temp);
+                }
+            }
+        }
+        return tempList.toArray(new String[0]);
     }
 
     public static String[] getCommonSubstrings(String string1, String string2) {
