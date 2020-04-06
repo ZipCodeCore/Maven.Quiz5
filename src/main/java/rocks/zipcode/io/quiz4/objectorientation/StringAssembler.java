ab616e4baccc7f5abf9dcf2ca059196586f61fdd
Mo Russ <mauriceruss302@gmail.com>
2020-04-06 12:10:06 -0400

    append test passed

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
@@ -4,14 +4,21 @@ package rocks.zipcode.io.quiz4.objectorientation;
  * @author leon on 11/12/2018.
  */
 public class StringAssembler {
+    Character delimeter;
+    String str;
     public StringAssembler(Character delimeter) {
+        this.delimeter = delimeter;
+        this.str = "";
     }
 
     public StringAssembler append(String str) {
-        return null;
+        this.str += str;
+        this.str += delimeter;
+
+        return this;
     }
 
     public String assemble() {
-        return null;
+        return str.substring(0,str.length()-1);
     }
 }
