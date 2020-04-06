6f160ab589f7171c85e43175daeb9c3a0f8109a8
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-04-06 09:56:27 -0400

    saving work so far

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,18 +1,49 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        int count = 0;
+        List<String> arrList = new ArrayList<>();
+        for (int i = 0; i <= string.length(); i++) {
+            for (int j = 0; j < string.length(); j++) {
+                if (isPalindrome(string.substring(i, j + 1))) {
+                    arrList.add(string.substring(i, j + 1));
+                    count++;
+                    System.out.println(string.substring(i, j + 1));
+                }
+            }
+        }
+        String[] arr = new String[count];
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arrList.get(i);
+        }
+        return arr;
     }
 
     public static Boolean isPalindrome(String string) {
-        return null;
+        int start = 0;
+        int end = string.length() - 1;
+
+        while (start < end) {
+            if (string.charAt(start) != string.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
     }
 
     public static String reverseString(String string) {
-        return null;
+        StringBuilder reversed = new StringBuilder();
+        reversed.append(string);
+        reversed.reverse();
+        return reversed.toString();
     }
 }
