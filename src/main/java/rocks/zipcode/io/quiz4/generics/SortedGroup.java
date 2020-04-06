af6dcc524fa150eec9a5b54099fae6cf756f2b7b
k-romero <imkevinromero@gmail.com>
2020-04-06 12:16:12 -0400

    checkpoint

--- src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
@@ -4,15 +4,34 @@ package rocks.zipcode.io.quiz4.generics;
  * @author leon on 18/12/2018.
  */
 public class SortedGroup<_> extends Group<_> {
+
     @Override
     public void insert(_ value) {
+        if(list.size() == 0){
+            list.add(value);
+        } else if(list.get(list.size()-1).toString().compareTo(value.toString()) == 0 ){
+            list.add(value);
+        } else if(list.get(list.size()-1).toString().compareTo(value.toString()) == -1 ){
+            list.add(value);
+        } else {
+            _ temp = list.get(list.size()-1);
+            list.remove(temp);
+            list.add(value);
+            list.add(temp);
+        }
     }
 
     @Override
     public void delete(_ value) {
+        list.remove(value);
     }
 
     public Integer indexOf(_ value) {
-        return null;
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i) == value){
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
@@ -1,21 +1,27 @@
 package rocks.zipcode.io.quiz4.objectorientation;
 
+import rocks.zipcode.io.quiz4.fundamentals.PalindromeEvaluator;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeObject {
+
+    String str;
+
     public PalindromeObject(String input) {
+        this.str = input;
     }
 
     public String[] getAllPalindromes(){
-        return null;
+        return PalindromeEvaluator.getAllPalindromes(str);
     }
 
     public Boolean isPalindrome(){
-        return null;
+        return PalindromeEvaluator.isPalindrome(str);
     }
 
     public String reverseString(){
-        return null;
+        return PalindromeEvaluator.reverseString(str);
     }
 }
