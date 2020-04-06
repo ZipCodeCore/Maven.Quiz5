29a8f4375b5df38ddfbf776274bfef3af15fe0e3
Greg Davis <jrobley3@hotmail.com>
2020-04-06 10:31:14 -0400

    get all substrings actually works

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -12,13 +12,15 @@ public class StringEvaluator {
     public static String[] getAllSubstrings(String string) {
         Set<String> subs = new HashSet<>();
         for (int i = 0; i < string.length(); i++) {
-            for (int j = 0; j < string.length(); j++) {
+            for (int j = i + 1; j <= string.length(); j++) {
                 subs.add(string.substring(i, j));
+                System.out.println(subs);
             }
         }
 //        Object[] result = subs.toArray();
 //        String[] answer = Arrays.copyOf(result,);
-        return null;
+        String[] answer = subs.toArray(new String[subs.size()]);
+        return answer;
     }
 
     public static String[] getCommonSubstrings(String string1, String string2) {
