6d98694922ab8e37399bb1c7d235b2a6e41d85dd
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-04-06 09:36:47 -0400

    first commit

--- src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
@@ -1,12 +1,17 @@
 package rocks.zipcode.io.quiz4.collections;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class ZipCodeWilmington {
+
+    List<Student> stud = new ArrayList<>();
     public void enroll(Student student) {
+
     }
 
     public Boolean isEnrolled(Student student) {
--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringEvaluatorObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringEvaluatorObject.java
@@ -4,7 +4,9 @@ package rocks.zipcode.io.quiz4.objectorientation;
  * @author leon on 19/12/2018.
  */
 public class StringEvaluatorObject {
+    String str;
     public StringEvaluatorObject(String str) {
+        this.str = str;
     }
 
     public String[] getAllSubstrings() {
