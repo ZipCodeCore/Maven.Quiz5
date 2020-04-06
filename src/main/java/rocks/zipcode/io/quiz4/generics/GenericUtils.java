d3b2b7f4e6e666af60c2dd5634fb71bb911acbbb
jc-hsiao <marshfire@live.com>
2020-04-06 13:05:46 -0400

    :(

--- src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
@@ -7,14 +7,10 @@ import java.util.*;
  */
 public class GenericUtils {
     public static <T extends Comparable<T>> Iterable<? extends Iterable<T>> powerSet(Set<T> originalSet) {
-
         long pow_set_size = (long)Math.pow(2, originalSet.size());
         int counter, j;
-
         Set<List<T>> result = new LinkedHashSet<>();
-
         ArrayList<T> a = new ArrayList<>(originalSet);
-
         for(counter = 0; counter < pow_set_size; counter++) {
             List<T> list = new ArrayList<>();
             for(j = 0; j < originalSet.size(); j++) {
@@ -23,7 +19,26 @@ public class GenericUtils {
             }
             result.add(list);
         }
+        if(originalSet.size()>2){
+            for(List<T> l : only2(result)){
+                T temp = l.get(0);
+                l.set(0,l.get(1));
+                l.set(1,temp);
+                result.add(l);
+            }
+        }
+
         return result;
+    }
+
+    public static <T extends Comparable<T>> Set<List<T>> only2 (Set<List<T>> set){
+        Set<List<T>> result = new HashSet<>();
+        for(List<T> l:set){
+            if(l.size()==2)
+                result.add(l);
+        }
+        return result;
+
     }
 
     public static <T extends Comparable<T>> Iterable<? extends Iterable<T>>powerSet(T[] arr) {
