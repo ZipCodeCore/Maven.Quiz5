9815bf89e7445f5f803dd325d75aebd629207317
Greg Davis <jrobley3@hotmail.com>
2020-04-06 10:23:11 -0400

    need to turn set into string array

--- src/main/java/rocks/zipcode/io/quiz4/collections/SimpleStringGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/SimpleStringGroup.java
@@ -1,15 +1,22 @@
 package rocks.zipcode.io.quiz4.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class SimpleStringGroup {
+    List<String> stringGroup = new ArrayList<>();
+    String string;
+
     public SimpleStringGroup() {
         throw new UnsupportedOperationException("Method not yet implemented");
     }
 
     public Integer count() {
-        return null;
+        stringGroup.add(string);
+        return stringGroup.size();
     }
 
     public void insert(String string) {
--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,10 +1,23 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
+
     public static String[] getAllSubstrings(String string) {
+        Set<String> subs = new HashSet<>();
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = 0; j < string.length(); j++) {
+                subs.add(string.substring(i, j));
+            }
+        }
+//        Object[] result = subs.toArray();
+//        String[] answer = Arrays.copyOf(result,);
         return null;
     }
 
