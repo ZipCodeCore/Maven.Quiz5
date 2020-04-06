28dad30238d68b9871a5dfff8b09dddb24d84ee6
muhammeta7 <muhammeta7@gmail.com>
2020-04-06 12:04:42 -0400

    generic utils, comparable treeset left

--- src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
@@ -9,6 +9,7 @@ import java.util.TreeSet;
  */
 public class GenericUtils {
     public static <_ extends Comparable> Iterable<? extends Iterable<_>> powerSet(Set<_> originalSet) {
+
         return null;
     }
 
