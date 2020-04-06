f77a30c42e94f2777a2d9c82201bc3f1036ce017
asconem <asconem@gmail.com>
2020-04-06 12:01:35 -0400

    Working on generics

--- src/main/java/rocks/zipcode/io/quiz4/generics/MyStack.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/MyStack.java
@@ -1,25 +1,39 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.Iterator;
+import java.util.Stack;
+
 /**
  * @author leon on 11/12/2018.
  */
-public class MyStack<SomeType> {
+public class MyStack<SomeType> implements Iterable<SomeType> {
+    private Stack newStack;
+
     public MyStack() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        this.newStack = new Stack();
     }
 
     public Boolean isEmpty() {
-        return null;
+        return newStack.isEmpty();
     }
 
     public void push(SomeType i) {
+        newStack.push(i);
     }
 
     public SomeType peek() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        if (isEmpty()){
+            return null;
+        }
+        return (SomeType) newStack.peek();
     }
 
     public SomeType pop() {
+        return (SomeType) newStack.pop();
+    }
+
+    @Override
+    public Iterator<SomeType> iterator() {
         return null;
     }
 }
