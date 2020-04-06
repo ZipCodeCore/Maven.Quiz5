f67e34020c799fcffa686f0eef13e821c370ab3e
cwilliams <coreywllms9@gmail.com>
2020-04-06 11:34:19 -0400

    string evaluator

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -6,10 +6,18 @@ package rocks.zipcode.io.quiz4.fundamentals;
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
 
+//        String myString = "anna";
+//        StringBuffer buffer = new StringBuffer(myString);
+//        buffer.reverse();
+//        String data = buffer.toString();
+//        if(myString.equals(data)){
+//            System.out.println("Given String is palindrome");
+//        } else {
+//            System.out.println("Given String is not palindrome");
+//        }
 
         return null;
-    }
-
+}
     public static Boolean isPalindrome(String string) {
 
         for(int i=0;i<=string.length()/2;i++)
@@ -20,6 +28,18 @@ public class PalindromeEvaluator {
     }
 
     public static String reverseString(String string) {
-        return null;
+
+//        String rev = string;
+//        int length = string.length();
+//
+//        for ( int i = length - 1; i >= 0; i-- )
+//            rev = rev + string.charAt(i);
+//
+//        return rev;
+    StringBuilder builder = new StringBuilder();
+        for(int i = string.length()-1; i >= 0 ; i--) {
+        builder.append(string.charAt(i));
     }
+        return builder.toString();
+}
 }
--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
@@ -12,10 +12,30 @@ public class PalindromeObject {
     }
 
     public Boolean isPalindrome(){
-        return null;
+        String clean = "".replaceAll("\\s+", "").toLowerCase();
+        int length = clean.length();
+        int forward = 0;
+        int backward = length - 1;
+        while (backward > forward) {
+            char forwardChar = clean.charAt(forward++);
+            char backwardChar = clean.charAt(backward--);
+            if (forwardChar != backwardChar)
+                return false;
+        }
+        return true;
+
+       // return null;
+
     }
 
     public String reverseString(){
-        return null;
+        StringBuilder reverse = new StringBuilder();
+        String clean = "".replaceAll("\\s+", "").toLowerCase();
+        char[] plain = clean.toCharArray();
+        for (int i = plain.length - 1; i >= 0; i--) {
+            reverse.append(plain[i]);
+        }
+        return String.valueOf((reverse.toString()).equals(clean));
+        //return null;
     }
 }
