7960b60d9f9137f8f03305b10b820ea4407f7f29
Mo Russ <mauriceruss302@gmail.com>
2020-04-06 11:58:07 -0400

    all object getAllPal tests passed

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
@@ -1,11 +1,15 @@
 package rocks.zipcode.io.quiz4.objectorientation;
 
+import rocks.zipcode.io.quiz4.fundamentals.PalindromeEvaluator;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeObject {
     private String input;
+    private PalindromeEvaluator ev = new PalindromeEvaluator();
     public PalindromeObject(String input) {
+        this.input = input;
     }
 
     public PalindromeObject(){
@@ -14,7 +18,7 @@ public class PalindromeObject {
 
     public String[] getAllPalindromes()
     {
-        return null;
+        return ev.getAllPalindromes(input);
     }
 
     public Boolean isPalindrome(){
