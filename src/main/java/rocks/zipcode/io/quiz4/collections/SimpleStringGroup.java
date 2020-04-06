7c7371d7b73b42c23d5f86b02f9d315391e715f6
aartikansal <aartilumb@gmail.com>
2020-04-06 10:40:46 -0400

    more tsts passed

--- src/main/java/rocks/zipcode/io/quiz4/collections/SimpleStringGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/SimpleStringGroup.java
@@ -1,31 +1,48 @@
 package rocks.zipcode.io.quiz4.collections;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @author leon on 11/12/2018.
  */
-public class SimpleStringGroup {
+public class SimpleStringGroup implements Iterable{
+    List<String> strList;
     public SimpleStringGroup() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        strList = new ArrayList<>();
+        //throw new UnsupportedOperationException("Method not yet implemented");
     }
 
     public Integer count() {
-        return null;
+        return strList.size();
     }
 
     public void insert(String string) {
+        strList.add(string);
     }
 
+
     public Boolean has(String string) {
-        return null;
+
+        return strList.contains(string);
     }
 
     public String fetch(int indexOfValue) {
-        return null;
+
+        return strList.get(indexOfValue);
     }
 
     public void delete(String string) {
+        strList.remove(string);
     }
 
     public void clear() {
+        strList.clear();
+    }
+
+    @Override
+    public Iterator iterator() {
+        return strList.iterator();
     }
 }
