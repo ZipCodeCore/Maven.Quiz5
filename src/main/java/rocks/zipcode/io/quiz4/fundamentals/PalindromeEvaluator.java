5838a2ab1079c030b5467b828750911bfac140dd
Mo Russ <mauriceruss302@gmail.com>
2020-04-06 11:51:47 -0400

    getAllPal test passed

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,11 +1,23 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        StringEvaluator ev = new StringEvaluator();
+
+        String [] allSubs= ev.getAllSubstrings(string);
+
+        ArrayList<String> list = new ArrayList<String>();
+        for(String s: allSubs)
+            if(isPalindrome(s) && !list.contains(s)){
+                list.add(s);
+            }
+
+        return list.toArray(new String[0]);
     }
 
     public static Boolean isPalindrome(String string) {
