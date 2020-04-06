213d273c9fe2d5f959f2923862fbc4daf18318e0
Han-Lin-zc <hlin04@gmail.com>
2020-04-06 11:34:02 -0400

    103/131 tests passed

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,11 +1,23 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < string.length(); i++) {
+            for (int k = string.length(); k > i; k--) {
+                if(isPalindrome(string.substring(i, k))) {
+                    if (!arrayList.contains(string.substring(i, k))) {
+                        arrayList.add(string.substring(i, k));
+                    }
+                }
+            }
+        }
+        return arrayList.toArray(new String[0]);
     }
 
     public static Boolean isPalindrome(String string) {
@@ -14,8 +26,8 @@ public class PalindromeEvaluator {
     }
 
     public static String reverseString(String string) {
-        StringBuilder plain = new StringBuilder(string);
-        StringBuilder reverse = plain.reverse();
+        StringBuilder sb = new StringBuilder(string);
+        StringBuilder reverse = sb.reverse();
         return reverse.toString();
     }
 }
