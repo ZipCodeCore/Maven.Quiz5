8baacd305b6edad5aab67545af1b2db5ceba6f39
muhammeta7 <muhammeta7@gmail.com>
2020-04-06 11:32:46 -0400

    all tests pass for object orientation

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
@@ -1,21 +1,30 @@
 package rocks.zipcode.io.quiz4.objectorientation;
 
+import rocks.zipcode.io.quiz4.fundamentals.PalindromeEvaluator;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeObject {
+
+    private String input;
+    private PalindromeEvaluator pe;
+
+
     public PalindromeObject(String input) {
+        this.input = input;
+        this.pe = new PalindromeEvaluator();
     }
 
     public String[] getAllPalindromes(){
-        return null;
+        return pe.getAllPalindromes(input);
     }
 
     public Boolean isPalindrome(){
-        return null;
+        return pe.isPalindrome(input);
     }
 
     public String reverseString(){
-        return null;
+        return pe.reverseString(input);
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringEvaluatorObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringEvaluatorObject.java
@@ -1,21 +1,29 @@
 package rocks.zipcode.io.quiz4.objectorientation;
 
+import rocks.zipcode.io.quiz4.fundamentals.StringEvaluator;
+
 /**
  * @author leon on 19/12/2018.
  */
 public class StringEvaluatorObject {
+
+    private String str;
+    private StringEvaluator se;
+
     public StringEvaluatorObject(String str) {
+        this.str = str;
+        this.se = new StringEvaluator();
     }
 
     public String[] getAllSubstrings() {
-        return null;
+        return se.getAllSubstrings(str);
     }
 
     public String[] getCommonSubstrings(String secondInput) {
-        return null;
+        return se.getCommonSubstrings(str, secondInput);
     }
 
     public String getLargestCommonSubstring(String secondInput) {
-        return null;
+        return se.getLargestCommonSubstring(str,secondInput);
     }
 }
