287a5939a5e7e5b22d4b12a5edef3a6aec827d09
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-04-06 12:15:02 -0400

    worked on generics

--- src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
@@ -1,18 +1,33 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class SortedGroup<_> extends Group<_> {
+    List<_> list;
+
+    public SortedGroup() {
+        this.list = new ArrayList<_>();
+    }
+
     @Override
     public void insert(_ value) {
+
+        this.list.add(value);
+
     }
 
     @Override
     public void delete(_ value) {
+
+        this.list.remove(value);
     }
 
     public Integer indexOf(_ value) {
-        return null;
+
+        return list.indexOf(value);
     }
 }
