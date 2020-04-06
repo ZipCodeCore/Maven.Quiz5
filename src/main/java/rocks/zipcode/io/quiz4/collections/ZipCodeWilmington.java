6899eccb2a7ceff1db0ca4f3d9baf9d1267c126b
Sandy Setiawan <ssetiawan@zipcodes-MBP-3.fios-router.home>
2020-04-06 12:34:01 -0400

    in progress

--- src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
@@ -1,22 +1,36 @@
 package rocks.zipcode.io.quiz4.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class ZipCodeWilmington {
+
+    List<Student> listOfStudents = new ArrayList<>();
+
     public void enroll(Student student) {
+        listOfStudents.add(student);
     }
 
     public Boolean isEnrolled(Student student) {
-        return null;
+        return listOfStudents.contains(student);
     }
 
     public void lecture(double numberOfHours) {
+        for (Student s : listOfStudents) {
+            s.learn(numberOfHours);
+        }
     }
 
     public Map<Student, Double> getStudyMap() {
-        return null;
+        Map<Student, Double> studyMap = new LinkedHashMap<>();
+        for (Student s : listOfStudents) {
+            studyMap.put(s, s.hours);
+        }
+        return studyMap;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
@@ -4,14 +4,19 @@ package rocks.zipcode.io.quiz4.objectorientation;
  * @author leon on 11/12/2018.
  */
 public class StringAssembler {
+    private Character delimter;
+    private String output = "";
+
     public StringAssembler(Character delimeter) {
+        this.delimter = delimeter;
     }
 
     public StringAssembler append(String str) {
-        return null;
+        output += str + delimter;
+        return this;
     }
 
     public String assemble() {
-        return null;
+        return output.substring(0, output.length() - 1);
     }
 }
