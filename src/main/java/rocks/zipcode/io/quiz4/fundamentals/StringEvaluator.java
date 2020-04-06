9006cbfe7c1d1748aa71501b669e00377a001ce6
aartikansal <aartilumb@gmail.com>
2020-04-06 10:20:29 -0400

    fundamentals string

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,12 +1,23 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.LinkedHashSet;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
     public static String[] getAllSubstrings(String string) {
-        return null;
+
+        LinkedHashSet<String> substrings =new LinkedHashSet<>();
+        for(int i =0; i< string.length();i++){
+            for(int j=i+1; j<= string.length(); j++){
+                substrings.add(string.substring(i,j));
+            }
+        }
+        return substrings.toArray(new String[1]);
     }
+
+
 
     public static String[] getCommonSubstrings(String string1, String string2) {
         return null;
